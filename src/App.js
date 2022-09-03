6f9ef55af34e8bdf24b05e79e2d9de6b09848978
karthik <tamakarthik@gmail.com>
2022-09-03 19:49:08 +0530

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        welcome
+        welcome karthik
         </p>
         <a
           className="App-link"
